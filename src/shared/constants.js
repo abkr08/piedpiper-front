6f9005d653f0f902ad4f6bcd9669d66dd765eceb
Abubakar Ibrahim <abubakaribrahim08@yahoo.com>
2020-05-29 19:19:11 +0300

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,6 @@
 const env = 'prod';
 const BASE_URL_DEV = 'http://localhost:8081/';
-const BASE_URL_PROD = 'https://api.piperchat.social';
+const BASE_URL_PROD = 'https://api.piperchat.social/';
 
 export const BASE_URL = env == 'dev' ? BASE_URL_DEV : BASE_URL_PROD;
  
